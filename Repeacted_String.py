a4ecd09f480ead92527628fad99249371aebeb16
jarvisbalu <poobalanbalu475@gmail.com>
2025-03-20 14:23:18 +0530

    Repeacted Letter

--- Repeacted_String.py
+++ Repeacted_String.py
@@ -1,3 +1,10 @@
 #To Print Number of Repeacted String
 txt= str(input("Enter the String: "))
 print(len(txt)-len(set(txt.lower())))
+
+#To Print Repeacted String
+rep=''
+for i in txt.lower():
+    if txt.lower().count(i)>1 and i not in rep:
+        rep+=i
+print("The Repeacted Letters is:", rep)
