0ec1a30af039ce927416d1347920c569a5e1dc43
jarvisbalu <poobalanbalu475@gmail.com>
2025-03-20 14:29:36 +0530

    Non-Repeacted Letter

--- Nonrepeacted_String.py
+++ Nonrepeacted_String.py
@@ -1,3 +1,10 @@
-#To Print Number of Repeacted String
+#To Print The Non-Repeacted String
 txt= str(input("Enter the string value: "))
 print(sum(1 for i in txt.lower() if txt.lower().count(i)==1))
+
+#To Print The Non-Repeacted Letter
+nrep=''
+for i in txt.lower():
+    if txt.lower().count(i)==1:
+        nrep+=i
+print("The Non-repeacted Letter is:", nrep)
